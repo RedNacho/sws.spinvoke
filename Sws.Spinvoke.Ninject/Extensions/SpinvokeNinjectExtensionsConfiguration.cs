dbf2c11c87181fdbe35975c8f09056aa8f4a933e
unknown-user <unknown-user@192.168.0.8>
2015-03-04 23:13:40 +0000

    Separated extensions config into its own class.

---
+++ Sws.Spinvoke.Ninject/Extensions/SpinvokeNinjectExtensionsConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Ninject;
+using Ninject.Infrastructure;
+
+using Sws.Spinvoke.Core;
+
+// PoC: Unit test, refactor.
+namespace Sws.Spinvoke.Ninject.Extensions
+{
+	public static class SpinvokeNinjectExtensionsConfiguration
+	{
+		public static void Configure (INativeLibraryLoader nativeLibraryLoader)
+		{
+			INativeDelegateResolver nativeDelegateResolver;
+
+			using (var kernel = new StandardKernel ()) {
+				kernel.Bind<INativeLibraryLoader> ().ToConstant (nativeLibraryLoader);
+				kernel.Load (new SpinvokeModule (StandardScopeCallbacks.Transient, "BindingToSyntaxExtensions"));
+				nativeDelegateResolver = kernel.Get<INativeDelegateResolver> ();
+			}
+
+			BindingToSyntaxExtensions.Configure (nativeDelegateResolver);
+		}
+	}
+}
+
