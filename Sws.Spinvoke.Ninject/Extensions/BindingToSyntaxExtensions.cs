aaa2aad2d6c50c074527bd193e28245d34cff235
unknown-user <unknown-user@192.168.0.8>
2015-03-04 22:23:06 +0000

    ToNative from non-generic Bind.

--- Sws.Spinvoke.Ninject/Extensions/BindingToSyntaxExtensions.cs
+++ Sws.Spinvoke.Ninject/Extensions/BindingToSyntaxExtensions.cs
@@ -40,29 +40,57 @@ namespace Sws.Spinvoke.Ninject.Extensions
 		public static IBindingWhenInNamedWithOrOnSyntax<T> ToNative<T>(this IBindingToSyntax<T> bindingToSyntax, string libraryName)
 			where T : class
 		{
+			Type serviceType;
+
+			var bindingBuilder = bindingToSyntax as BindingBuilder<T>;
+
+			if (bindingBuilder == null) {
+				serviceType = typeof(T);
+			} else {
+				serviceType = bindingBuilder.Binding.Service;
+			}
+
+			return ToNative (bindingToSyntax, serviceType, libraryName);
+		}
+
+		public static IBindingWhenInNamedWithOrOnSyntax<T> ToNative<T>(this IBindingToSyntax<T> bindingToSyntax, Type serviceType, string libraryName)
+			where T : class
+		{
 			VerifyConfigured ();
 
-			bindingToSyntax.BindingConfiguration.ProviderCallback = context => new NativeProxyProvider<T> (libraryName);
+			bindingToSyntax.BindingConfiguration.ProviderCallback = context => new NativeProxyProvider<T> (serviceType, libraryName);
 
-			return new BindingConfigurationBuilder<T> (bindingToSyntax.BindingConfiguration, typeof(T).Format(), bindingToSyntax.Kernel);
+			return new BindingConfigurationBuilder<T> (bindingToSyntax.BindingConfiguration, serviceType.Format(), bindingToSyntax.Kernel);
 		}
 
 		private class NativeProxyProvider<T> : Provider<T>
 			where T : class
 		{
+			private readonly Type _serviceType;
+
 			private readonly string _libraryName;
 
-			public NativeProxyProvider(string libraryName)
+			public NativeProxyProvider(Type serviceType, string libraryName)
 			{
+				if (serviceType == null)
+					throw new ArgumentNullException("serviceType");
+
 				if (libraryName == null)
 					throw new ArgumentNullException("libraryName");
 
+				_serviceType = serviceType;
 				_libraryName = libraryName;
 			}
 
 			protected override T CreateInstance (IContext context)
 			{
-				return ProxyGenerator.CreateInterfaceProxyWithoutTarget<T> (new NativeDelegateInterceptor (_libraryName, NativeDelegateResolver));
+				var nativeDelegateInterceptor = new NativeDelegateInterceptor (_libraryName, NativeDelegateResolver);
+
+				if (_serviceType == typeof(T)) {
+					return ProxyGenerator.CreateInterfaceProxyWithoutTarget<T> (nativeDelegateInterceptor);
+				} else {
+					return ProxyGenerator.CreateInterfaceProxyWithoutTarget (_serviceType, nativeDelegateInterceptor) as T;
+				}
 			}
 		}
 	}
