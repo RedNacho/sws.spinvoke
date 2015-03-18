f9afdc92ad975395b24ba54bbeacc6d93260a2d4
RedNachoBitbucket <rednachobitbucket@example.com>
2015-03-18 16:24:58 +0100

    Added missing AttributeUsage value.

--- Sws.Spinvoke.Interception/NativeReturnsStringPointerAttribute.cs
+++ Sws.Spinvoke.Interception/NativeReturnsStringPointerAttribute.cs
@@ -4,6 +4,7 @@ using Sws.Spinvoke.Interception.ReturnPostprocessing;
 
 namespace Sws.Spinvoke.Interception
 {
+	[AttributeUsage(AttributeTargets.ReturnValue)]
 	public class NativeReturnsStringPointerAttribute : NativeReturnDefinitionOverrideAttribute
 	{
 		public NativeReturnsStringPointerAttribute()
