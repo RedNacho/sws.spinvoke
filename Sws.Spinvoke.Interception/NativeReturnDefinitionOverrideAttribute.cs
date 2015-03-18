5689ca43410260d038e9f242415cc767627469cc
unknown-user <unknown-user@192.168.2.131>
2015-03-18 19:26:49 +0100

    Locking down attribute base classes.

--- Sws.Spinvoke.Interception/NativeReturnDefinitionOverrideAttribute.cs
+++ Sws.Spinvoke.Interception/NativeReturnDefinitionOverrideAttribute.cs
@@ -5,13 +5,13 @@ using Sws.Spinvoke.Interception.ReturnPostprocessing;
 namespace Sws.Spinvoke.Interception
 {
 	[AttributeUsage(AttributeTargets.ReturnValue)]
-	public class NativeReturnDefinitionOverrideAttribute : Attribute
+	public abstract class NativeReturnDefinitionOverrideAttribute : Attribute
 	{
 		private readonly IReturnPostprocessor _returnPostprocessor;
 
 		private readonly Type _outputType;
 
-		public NativeReturnDefinitionOverrideAttribute(IReturnPostprocessor returnPostprocessor, Type outputType = null)
+		protected NativeReturnDefinitionOverrideAttribute(IReturnPostprocessor returnPostprocessor, Type outputType = null)
 		{
 			if (returnPostprocessor == null) {
 				throw new ArgumentNullException("returnPostprocessor");
