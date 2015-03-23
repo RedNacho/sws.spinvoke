04534d7e24f7d22c793840269bc570d5aa279b49
unknown-user <unknown-user@192.168.2.131>
2015-03-23 18:25:15 +0100

    Refactoring; tentative support for Func/Action.

---
+++ Sws.Spinvoke.Core.Tests/Delegates/Generics/DefaultGenericDelegateTypeConverterTests.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+using Moq;
+
+using Sws.Spinvoke.Core.Delegates.Generics;
+
+namespace Sws.Spinvoke.Core.Tests
+{
+	[TestFixture ()]
+	public class DefaultGenericDelegateTypeConverterTests
+	{
+		public delegate void TestDelegate();
+
+		[Test ()]
+		public void ConvertsActionToDelegate ()
+		{
+			var delegateTypeProviderMock = new Mock<IDelegateTypeProvider>();
+
+			delegateTypeProviderMock.Setup(dtp => dtp.GetDelegateType(It.IsAny<DelegateSignature>()))
+				.Returns(typeof(TestDelegate));
+
+			var subject = new DefaultGenericDelegateTypeConverter(delegateTypeProviderMock.Object);
+
+			var convertedDelegateType = subject.ConvertToInteropSupportedDelegateType (typeof(Action<int, string>), CallingConvention.Cdecl);
+
+			Assert.AreEqual (typeof(TestDelegate), convertedDelegateType);
+
+			delegateTypeProviderMock.Verify(dtp => dtp.GetDelegateType(It.Is<DelegateSignature>(ds => ds.CallingConvention == CallingConvention.Cdecl)), Times.Once);
+			delegateTypeProviderMock.Verify(dtp => dtp.GetDelegateType(It.Is<DelegateSignature>(ds => ds.InputTypes.SequenceEqual(new [] { typeof(int), typeof(string) }))), Times.Once);
+			delegateTypeProviderMock.Verify(dtp => dtp.GetDelegateType(It.Is<DelegateSignature>(ds => ds.OutputType == typeof(void))), Times.Once);
+
+			delegateTypeProviderMock.Verify (dtp => dtp.GetDelegateType (It.IsAny<DelegateSignature> ()), Times.Once);
+		}
+
+		[Test ()]
+		public void ConvertsFuncToDelegate ()
+		{
+			var delegateTypeProviderMock = new Mock<IDelegateTypeProvider>();
+
+			delegateTypeProviderMock.Setup(dtp => dtp.GetDelegateType(It.IsAny<DelegateSignature>()))
+				.Returns(typeof(TestDelegate));
+
+			var subject = new DefaultGenericDelegateTypeConverter(delegateTypeProviderMock.Object);
+
+			var convertedDelegateType = subject.ConvertToInteropSupportedDelegateType (typeof(Func<int, string, double>), CallingConvention.Cdecl);
+
+			Assert.AreEqual (typeof(TestDelegate), convertedDelegateType);
+
+			delegateTypeProviderMock.Verify(dtp => dtp.GetDelegateType(It.Is<DelegateSignature>(ds => ds.CallingConvention == CallingConvention.Cdecl)), Times.Once);
+			delegateTypeProviderMock.Verify(dtp => dtp.GetDelegateType(It.Is<DelegateSignature>(ds => ds.InputTypes.SequenceEqual(new [] { typeof(int), typeof(string) }))), Times.Once);
+			delegateTypeProviderMock.Verify(dtp => dtp.GetDelegateType(It.Is<DelegateSignature>(ds => ds.OutputType == typeof(double))), Times.Once);
+
+			delegateTypeProviderMock.Verify (dtp => dtp.GetDelegateType (It.IsAny<DelegateSignature> ()), Times.Once);
+		}
+	}
+}
+
+
---
+++ Sws.Spinvoke.Core/Delegates/Generics/DefaultGenericDelegateTypeConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace Sws.Spinvoke.Core.Delegates.Generics
+{
+	public class DefaultGenericDelegateTypeConverter : IGenericDelegateTypeConverter
+	{
+		private readonly IDelegateTypeProvider _delegateTypeProvider;
+
+		public DefaultGenericDelegateTypeConverter (IDelegateTypeProvider delegateTypeProvider)
+		{
+			if (delegateTypeProvider == null)
+			{
+				throw new ArgumentNullException ("delegateTypeProvider");
+			}
+
+			_delegateTypeProvider = delegateTypeProvider;
+		}
+
+		public Type ConvertToInteropSupportedDelegateType (Type genericDelegateType, CallingConvention callingConvention)
+		{
+			var method = genericDelegateType.GetMethod ("Invoke");
+
+			var delegateSignature = new DelegateSignature (method.GetParameters ().Select (parameterInfo => parameterInfo.ParameterType).ToArray (), method.ReturnType, callingConvention);
+
+			return _delegateTypeProvider.GetDelegateType (delegateSignature);
+		}
+	}
+}
+
