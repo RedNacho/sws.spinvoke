07cf61e44cef9d00df74979c1583d19389efc02c
RedNachoBitbucket <rednachobitbucket@example.com>
2015-03-23 17:34:18 +0100

    Adding support for function callbacks.

---
+++ Sws.Spinvoke.Interception/ArgumentPreprocessing/DelegateToPointerArgumentPreprocessor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Sws.Spinvoke.Interception.ArgumentPreprocessing
+{
+	public class DelegateToPointerArgumentPreprocessor : IArgumentPreprocessor
+	{
+		private readonly PointerManagementMode _pointerManagementMode;
+
+		public DelegateToPointerArgumentPreprocessor(PointerManagementMode pointerManagementMode)
+		{
+			_pointerManagementMode = pointerManagementMode;
+		}
+
+		public bool CanProcess (object input)
+		{
+			return input is Delegate;
+		}
+
+		public object Process (object input)
+		{
+			return Marshal.GetFunctionPointerForDelegate ((Delegate)input);
+		}
+
+		public void ReleaseProcessedInput (object processedInput)
+		{
+		}
+	}
+}
+
