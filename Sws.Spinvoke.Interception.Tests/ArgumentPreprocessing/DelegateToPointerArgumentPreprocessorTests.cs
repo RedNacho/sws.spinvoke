c46ff7d71c9f2082e6366badcb32a1510382e146
unknown-user <unknown-user@192.168.2.131>
2015-03-23 17:34:18 +0100

    Adding support for function callbacks.

---
+++ Sws.Spinvoke.Interception.Tests/ArgumentPreprocessing/DelegateToPointerArgumentPreprocessorTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Runtime.InteropServices;
+
+using Sws.Spinvoke.Interception.ArgumentPreprocessing;
+
+namespace Sws.Spinvoke.Interception.Tests
+{
+	[TestFixture ()]
+	public class DelegateToPointerArgumentPreprocessorTests
+	{
+		[Test ()]
+		public void CanProcessReturnsTrueForDelegate()
+		{
+			var subject = new DelegateToPointerArgumentPreprocessor (PointerManagementMode.DoNotDestroy);
+
+			Func<int, int> addOne = i => i + 1;
+
+			var canProcess = subject.CanProcess (addOne);
+
+			Assert.IsTrue (canProcess);
+		}
+
+		[Test ()]
+		public void CanProcessReturnsFalseForNonDelegate()
+		{
+			var subject = new DelegateToPointerArgumentPreprocessor (PointerManagementMode.DoNotDestroy);
+
+			var canProcess = subject.CanProcess (new object ());
+
+			Assert.IsFalse (canProcess);
+		}
+
+		[Test ()]
+		public void DelegateCanBeMarshaledBackFromPointer()
+		{
+			var subject = new DelegateToPointerArgumentPreprocessor (PointerManagementMode.DoNotDestroy);
+
+			Func<int, int> addOne = i => i + 1;
+
+			var ptr = (IntPtr) subject.Process (addOne);
+
+			var delegateFromPtr = Marshal.GetDelegateForFunctionPointer (ptr, typeof(AddOneDelegate));
+
+			var result = delegateFromPtr.DynamicInvoke (new object[] { 7 });
+
+			Assert.AreEqual (8, result);
+		}
+
+		public delegate int AddOneDelegate(int i);
+	}
+}
+
---
+++ Sws.Spinvoke.Interception/NativeArgumentAsFunctionPointerAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Sws.Spinvoke.Interception.ArgumentPreprocessing;
+
+namespace Sws.Spinvoke.Interception
+{
+	public class NativeArgumentAsFunctionPointerAttribute : NativeArgumentDefinitionOverrideAttribute
+	{
+		public NativeArgumentAsFunctionPointerAttribute (PointerManagementMode pointerManagementMode)
+			: base(new DelegateToPointerArgumentPreprocessor(pointerManagementMode), typeof(Delegate))
+		{
+		}
+	}
+}
+
