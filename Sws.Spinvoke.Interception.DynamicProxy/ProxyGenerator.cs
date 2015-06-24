cdd1ea4fc9667024ab3976551cc713b9fe84abde
simon <unknown-user@192.168.1.3>
2015-06-24 18:16:28 +0100

    Missing file. :(

---
+++ Sws.Spinvoke.Interception.DynamicProxy/ProxyGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using DynamicProxyGenerator = Castle.DynamicProxy.ProxyGenerator;
+
+namespace Sws.Spinvoke.Interception.DynamicProxy
+{
+	public class ProxyGenerator : IProxyGenerator
+	{
+		private readonly DynamicProxyGenerator _sourceProxyGenerator;
+
+		public ProxyGenerator(DynamicProxyGenerator sourceProxyGenerator)
+		{
+			if (sourceProxyGenerator == null) {
+				throw new ArgumentNullException ("sourceProxyGenerator");
+			}
+
+			_sourceProxyGenerator = sourceProxyGenerator;
+		}
+
+		public bool AllowsTarget
+		{
+			get { return true; }
+		}
+
+		public T CreateProxy<T> (IInterceptor interceptor) where T : class
+		{
+			return _sourceProxyGenerator.CreateInterfaceProxyWithoutTarget<T> (new SpinvokeInterceptor (interceptor));
+		}
+
+		public object CreateProxy (Type interfaceToProxy, IInterceptor interceptor)
+		{
+			return _sourceProxyGenerator.CreateInterfaceProxyWithoutTarget (interfaceToProxy, new SpinvokeInterceptor (interceptor));
+		}
+
+		public T CreateProxyWithTarget<T>(IInterceptor interceptor, T target) where T : class
+		{
+			return _sourceProxyGenerator.CreateInterfaceProxyWithTarget<T> (target, new SpinvokeInterceptor(interceptor));
+		}
+
+		public object CreateProxyWithTarget(Type interfaceToProxy, IInterceptor interceptor, object target)
+		{
+			return _sourceProxyGenerator.CreateInterfaceProxyWithTarget (interfaceToProxy, target, new SpinvokeInterceptor(interceptor));
+		}
+	}
+}
+
