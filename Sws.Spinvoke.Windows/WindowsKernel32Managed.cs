96b73dc7eaea86c8955545109f55565aebbf229d
RedNachoBitbucket <rednachobitbucket@example.com>
2015-11-27 23:10:49 +0100

    Moving Kernel32 logic into core.

---
+++ Sws.Spinvoke.Windows/WindowsKernel32Managed.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Sws.Spinvoke.Core.LibraryLoading;
+using System.Runtime.InteropServices;
+using Sws.Spinvoke.Core;
+
+namespace Sws.Spinvoke.Windows
+{
+	public class WindowsKernel32Managed : IKernel32Managed<WindowsKernel32Managed.WindowsSafeLibraryHandle>
+	{
+		public WindowsSafeLibraryHandle LoadLibrary (string fileName)
+		{
+			return NativeMethods.LoadLibrary (fileName);
+		}
+
+		public IntPtr GetProcAddress (SafeHandle hModule, string procedureName)
+		{
+			return NativeMethods.GetProcAddress (hModule, procedureName);
+		}
+
+		private static class NativeMethods
+		{
+			[DllImport("kernel32.dll")]
+			public static extern WindowsSafeLibraryHandle LoadLibrary(string dllToLoad);
+
+			[DllImport("kernel32.dll")]
+			public static extern IntPtr GetProcAddress(SafeHandle hModule, string procedureName);
+
+			[DllImport("kernel32.dll")]
+			public static extern bool FreeLibrary(IntPtr hModule);
+		}
+
+		public sealed class WindowsSafeLibraryHandle : SafeLibraryHandle
+		{
+			protected override bool ReleaseHandle ()
+			{
+				return NativeMethods.FreeLibrary (handle);
+			}
+		}
+	}
+}
+
