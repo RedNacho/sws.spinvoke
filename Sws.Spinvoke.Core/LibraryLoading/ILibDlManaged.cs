7b95b8ff145a2b7d80964a881af8d5073b961873
RedNachoBitbucket <rednachobitbucket@example.com>
2015-11-27 20:23:03 +0100

    Moving libdl logic into Core.

---
+++ Sws.Spinvoke.Core/LibraryLoading/ILibDlManaged.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Sws.Spinvoke.Core.LibraryLoading
+{
+	public interface ILibDlManaged<TSafeLibraryHandle>
+		where TSafeLibraryHandle : SafeLibraryHandle
+	{
+		TSafeLibraryHandle DlOpen(string filename, int flags);
+		IntPtr DlError();
+		IntPtr DlSym(SafeHandle handle, string symbol);
+		string StringFromDlError(IntPtr dlErrorPtr);
+	}
+}
--- Sws.Spinvoke.Linux/LinuxNativeLibraryLoader.cs
+++ Sws.Spinvoke.Linux/LinuxNativeLibraryLoader.cs
@@ -3,59 +3,14 @@ using System.Runtime.InteropServices;
 using Sws.Spinvoke.Core;
 using Sws.Spinvoke.Core.Exceptions;
 
-// This is really easy to implement for Windows.
-// I just haven't done it because I can't test it right now.
+using Sws.Spinvoke.Core.LibraryLoading;
+
 namespace Sws.Spinvoke.Linux
 {
-	public class LinuxNativeLibraryLoader : INativeLibraryLoader
+	public class LinuxNativeLibraryLoader : LibDlNativeLibraryLoader<LinuxLibDlManaged.LinuxSafeLibraryHandle>
 	{
-		private const int RTLD_NOW = 2;
-
-		public SafeLibraryHandle LoadLibrary (string fileName)
+		public LinuxNativeLibraryLoader() : base(new LinuxLibDlManaged())
 		{
-			return dlopen (fileName, RTLD_NOW);
-		}
-
-		public void UnloadLibrary (SafeLibraryHandle libHandle)
-		{
-			libHandle.Close ();
-		}
-
-		public IntPtr GetFunctionPointer (SafeLibraryHandle libHandle, string functionName)
-		{
-			dlerror ();
-
-			var res = dlsym (libHandle, functionName);
-
-			var errPtr = dlerror ();
-
-			if (errPtr != IntPtr.Zero) {
-				throw new NativeLibraryLoadException (string.Format ("dlsym errored trying to load function {0}.  Error was: {1}", functionName, Marshal.PtrToStringAuto(errPtr)));
-			}
-
-			return res;
-		}
-
-		[DllImport("libdl.so")]
-		private static extern LinuxSafeLibraryHandle dlopen(string fileName, int flags);
-
-		[DllImport("libdl.so")]
-		private static extern int dlclose(IntPtr handle);
-
-		[DllImport("libdl.so")]
-		private static extern IntPtr dlerror();
-
-		[DllImport("libdl.so")]
-		private static extern IntPtr dlsym(SafeHandle handle, string symbol);
-
-		private sealed class LinuxSafeLibraryHandle : SafeLibraryHandle
-		{
-			protected override bool ReleaseHandle ()
-			{
-				var result = dlclose (handle);
-
-				return (result == 0);
-			}
 		}
 	}
 }
