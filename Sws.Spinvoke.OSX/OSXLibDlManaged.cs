2656ed701490ae52694dcb0fb36989b20e1d8fa5
simonwhite <unknown-user@192.168.1.9>
2015-11-27 20:46:53 +0100

    Adding OSXNativeLibraryLoader and tests.

---
+++ Sws.Spinvoke.OSX/OSXLibDlManaged.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Sws.Spinvoke.Core.LibraryLoading;
+using System.Runtime.InteropServices;
+using Sws.Spinvoke.Core;
+
+namespace Sws.Spinvoke.OSX
+{
+	public class OSXLibDlManaged : ILibDlManaged<OSXLibDlManaged.OSXSafeLibraryHandle>
+	{
+		public OSXSafeLibraryHandle DlOpen (string filename, int flags)
+		{
+			return dlopen (filename, flags);
+		}
+
+		public int DlClose (IntPtr handle)
+		{
+			return dlclose (handle);
+		}
+
+		public IntPtr DlError ()
+		{
+			return dlerror ();
+		}
+
+		public IntPtr DlSym (SafeHandle handle, string symbol)
+		{
+			return dlsym (handle, symbol);
+		}
+
+		public string StringFromDlError (IntPtr dlErrorPtr)
+		{
+			return Marshal.PtrToStringAuto (dlErrorPtr);
+		}
+
+		[DllImport("libdl.dylib")]
+		private static extern OSXSafeLibraryHandle dlopen(string fileName, int flags);
+
+		[DllImport("libdl.dylib")]
+		private static extern int dlclose(IntPtr handle);
+
+		[DllImport("libdl.dylib")]
+		private static extern IntPtr dlerror();
+
+		[DllImport("libdl.dylib")]
+		private static extern IntPtr dlsym(SafeHandle handle, string symbol);
+
+		public sealed class OSXSafeLibraryHandle : SafeLibraryHandle
+		{
+			protected override bool ReleaseHandle ()
+			{
+				var result = dlclose (handle);
+
+				return (result == 0);
+			}
+		}
+	}
+}
+
