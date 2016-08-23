199ff0cbdde0fae1189195929568c12e027e7e4c
simon <RedNacho@example.com>
2016-08-23 20:29:35 +0200

    Pointless commenting.

--- Sws.Spinvoke/Interception/NativeArgumentAsInteropCompatibleDelegateAttribute.cs
+++ Sws.Spinvoke/Interception/NativeArgumentAsInteropCompatibleDelegateAttribute.cs
@@ -29,7 +29,20 @@ namespace Sws.Spinvoke.Interception
 			}
 		}
 
-		public static DelegateToInteropCompatibleDelegateArgumentPreprocessor.IContextCustomisation CreateContextCustomisation (IDelegateTypeToDelegateSignatureConverter delegateTypeToDelegateSignatureConverter,
+		/// <summary>
+		/// This allows you to create a customisation for ArgumentPreprocessorContext with which
+		/// this attribute can work. If you override the ArgumentPreprocessorContextCustomiser
+		/// passed to the NativeDelegateInterceptor so that it either implements this interface
+		/// directly, or is customised via the .Customise method, this enables you to inject
+		/// runtime dependencies and settings required for the interop-compatible delegate conversion.
+		/// </summary>
+		/// <returns>The context customisation.</returns>
+		/// <param name="delegateTypeToDelegateSignatureConverter">Required; see Ninject SpinvokeModule for example</param>
+		/// <param name="delegateTypeProvider">Required; see Ninject SpinvokeModule for example</param>
+		/// <param name="callingConvention">The calling convention to use when creating the new delegate. If not supplied: The value explicitly passed the attribute takes precedence, and the fallback is then the calling convention of the method itself. This should normally be correct, so this parameter will normally be null.</param>
+		/// <param name="delegateRegistrationAction">Use this to register the created delegate so that it will not be garbage-collected too soon. By default, the delegate only survives for the duration of the native method call.</param>
+		public static DelegateToInteropCompatibleDelegateArgumentPreprocessor.IContextCustomisation CreateContextCustomisation (
+			IDelegateTypeToDelegateSignatureConverter delegateTypeToDelegateSignatureConverter,
 			IDelegateTypeProvider delegateTypeProvider,
 			CallingConvention? callingConvention = null,
 			Action<Delegate> delegateRegistrationAction = null) {
